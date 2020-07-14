26bcf08d603de551553b4e840a5cee2aca2a42eb
yangchen <chen.yang1@thoughtworks.com>
2020-07-14 17:31:45 +0800

    implements SquenceIterator to create a sequence without putting all items into memory

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/Sequence.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/Sequence.java
@@ -2,14 +2,18 @@ package com.cultivation.javaBasic.showYourIntelligence;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Sequence implements Iterable<Integer> {
     private final Integer start;
     private final Integer end;
 
     public Sequence(Integer start, Integer end) {
-        if (start >= end) { throw new IllegalArgumentException("Start must be smaller than End."); }
+        if (start >= end) {
+            throw new IllegalArgumentException("Start must be smaller than End.");
+        }
         this.start = start;
         this.end = end;
     }
@@ -23,13 +27,24 @@ public class Sequence implements Iterable<Integer> {
 class SequenceIterator implements Iterator<Integer> {
     // TODO: You can add additional fields or methods if you want.
     // <--start
+    private List<Integer> storage;
+    private int index;
 
+    private void initStorage(Integer start, Integer end) {
+        while (start < end) {
+            storage.add(start);
+            start++;
+        }
+    }
     // --end-->
 
     SequenceIterator(Integer start, Integer end) {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        storage = new ArrayList<>();
+        initStorage(start, end);
+        index = 0;
         // --end-->
     }
 
@@ -37,7 +52,8 @@ class SequenceIterator implements Iterator<Integer> {
     public boolean hasNext() {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        return index < storage.size();
         // --end-->
     }
 
@@ -45,7 +61,8 @@ class SequenceIterator implements Iterator<Integer> {
     public Integer next() {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        return storage.get(index++);
         // --end-->
     }
 }
